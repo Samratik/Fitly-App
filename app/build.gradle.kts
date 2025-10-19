e66eae3fdcf8730fb4fd6116590a11cda6cf5a0e
Samratik <samrat.kakimgali05@gmail.com>
2025-10-19 14:11:50 +0500

    Added ML Kit Pose Detection (working version 17.0.0)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,25 +57,18 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 
-    // Compose (обычно уже есть после шаблона)
-    implementation(platform("androidx.compose:compose-bom:2024.10.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.activity:activity-compose:1.9.3")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
-
-    // Navigation Compose
+    // Navigation
     implementation("androidx.navigation:navigation-compose:2.8.3")
 
     // CameraX
-    val camerax = "1.3.4" // или новее
+    val camerax = "1.3.4"
     implementation("androidx.camera:camera-core:$camerax")
     implementation("androidx.camera:camera-camera2:$camerax")
     implementation("androidx.camera:camera-lifecycle:$camerax")
     implementation("androidx.camera:camera-view:$camerax")
 
-    implementation("com.google.mlkit:pose-detection:18.0.0")
-    implementation("com.google.mlkit:pose-detection-accurate:18.0.0") // точнее, но медленнее
+    implementation("com.google.mlkit:pose-detection:17.0.0")
+
 
 
 }
